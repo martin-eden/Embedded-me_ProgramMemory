14d08e004e903f99f55c7b42d9f22b4b2917799c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-30 17:32:31 +0200

    [~-] [Example] It's test, minimized code to use just core modules.

--- examples/ProgramDumper/ProgramDumper.ino
+++ examples/ProgramDumper/ProgramDumper.ino
@@ -2,13 +2,12 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-08-29
+  Last mod.: 2025-08-30
 */
 
 #include <me_ProgramMemory.h>
 
 #include <me_BaseTypes.h>
-#include <me_BaseInterfaces.h>
 #include <me_Console.h>
 
 /*
@@ -16,61 +15,27 @@
 */
 void DumpFlash()
 {
-  /*
-    Idea in pseudocode is
-
-      addr = 0
-      while GetByte(b, addr)
-        print b
-        addr = addr + 1
-  */
-
-  const TUint_1 NumColumns = 16;
-
-  TAddressSegment FlashSeg = { .Addr = 0, .Size = TUint_2_Max };
-  TAddressIterator Rator;
-
-  TUint_1 Column;
-  TAddress Addr;
   TUint_1 Byte;
 
-  if (!Rator.Init(FlashSeg))
+  for (TAddress Addr = 0; true; ++Addr)
   {
-    Console.Print("Iterator initialization failed.");
-    return;
-  }
-
-  Console.Print("(");
-  Console.Indent();
-
-  Column = 1;
-
-  while (Rator.GetNextAddr(&Addr))
-  {
-    if (!me_ProgramMemory::GetByteFrom(&Byte, Addr))
+    if (!me_ProgramMemory::GetByteAt(&Byte, Addr))
       break;
 
     Console.Print(Byte);
-
-    ++Column;
-
-    if (Column > NumColumns)
-    {
-      Console.EndLine();
-      Column = 1;
-    }
   }
-
   Console.EndLine();
-  Console.Unindent();
-  Console.Print(")");
 }
 
 void setup()
 {
   Console.Init();
 
+  Console.Print("( [me_ProgramMemory] test");
+
   DumpFlash();
+
+  Console.Print(") Done");
 }
 
 void loop()
@@ -80,4 +45,5 @@ void loop()
 /*
   2024 # # #
   2025-08-22
+  2025-08-30
 */
